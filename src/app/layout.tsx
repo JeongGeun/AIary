9b966adc64ae05271fc4bf9fba180f846be16bb6
jeonggeun-jo <jeonggeun.jo@linecorp.com>
2025-06-23 00:08:45 +0900

    feat: add viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,14 @@
+import type { Viewport } from 'next';
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  minimumScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  viewportFit: 'cover',
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
