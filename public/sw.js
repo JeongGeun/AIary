679fc95c1f0f5b8fd1780579a9a60bf29772558b
jeonggeun-jo <jeonggeun.jo@linecorp.com>
2025-06-24 21:52:52 +0900

    fix: sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,24 +1,28 @@
 const CACHE_NAME = 'ai-ary-v1';
 const urlsToCache = ['/', '/web-app-manifest-192x192.png', '/manifest.json'];
 
-// 설치
+// 설치 - skipWaiting() 추가
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting()) // 이 부분만 추가
   );
 });
 
-// 활성화
+// 활성화 - clients.claim() 추가
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
+      return Promise.all([
+        ...cacheNames.map((cacheName) => {
           if (cacheName !== CACHE_NAME) {
             return caches.delete(cacheName);
           }
-        })
-      );
+        }),
+        self.clients.claim(), // 이 부분만 추가
+      ]);
     })
   );
 });
@@ -31,6 +35,7 @@ self.addEventListener('push', function (event) {
       icon: data.icon || '/web-app-manifest-192x192.png',
       badge: '/web-app-manifest-192x192.png',
       vibrate: [100, 50, 100],
+      requireInteraction: true, // 이 부분만 추가
       data: {
         dateOfArrival: Date.now(),
         primaryKey: '2',
