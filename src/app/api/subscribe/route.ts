68eb7095fd97e9ac038be269878ff87284748016
jeonggeun-jo <jeonggeun.jo@linecorp.com>
2025-06-26 22:12:27 +0900

    feat: add Response.json

--- src/app/api/subscribe/route.ts
+++ src/app/api/subscribe/route.ts
@@ -1,4 +1,3 @@
-import { NextResponse } from 'next/server';
 import { SimplePushManager } from '../_utils/PushManager';
 
 export const POST = async (request: Request) => {
@@ -8,12 +7,15 @@ export const POST = async (request: Request) => {
   console.log('Received body:', subscriptionData);
 
   // Return the result in the response
-  return new NextResponse(JSON.stringify({ result }), {
-    status: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-    },
-  });
+  return Response.json(
+    { result },
+    {
+      status: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      },
+    }
+  );
 };
